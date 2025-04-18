7aa1a863144474ac2e9f08ed1302bd67b977c5f2
djossir <autry2293@gmail.com>
2025-04-17 21:51:51 -0500

    Refactor Window class to integrate SFML graphics
    Updated Window.h and Window.cpp to utilize sf::RenderWindow,
    adding platform-specific headers and renaming member variables
    for consistency. Modified project files to include new build
    configurations and SFML dependencies, ensuring proper linking
    and inclusion of necessary DLLs.
    
    x64 is working fine, but x86 is having trouble finding graphics.hpp and blender..something

--- Engine/headers/Window.h
+++ Engine/headers/Window.h
@@ -1,51 +1,56 @@
-#ifndef WINDOW_H
-#define WINDOW_H
+#ifndef WINDOW_H  
+#define WINDOW_H  
 
-#include <iostream>
+#if defined(_WIN64)  
+	#include <SFML/Graphics.hpp>  
+	#include <../../../SFML/x64/include/SFML/Graphics.hpp>  
+#elif defined(__APPLE__) || defined(__MACH__)  
+   #include <SFML/Graphics.hpp> // Adjusted to use the default include path for macOS  
+#elif defined(__linux__)  
+   #include <../../../SFML/x86_64-unknown-linux-gnu/include/SFML/Graphics.hpp>  
+#elif defined(_WIN32)  
+	#include <../../../SFML/x86/include/SFML/Graphics.hpp>
+#else
+	#error "Unsupported platform. Please adjust the include paths for your system."
 
-class Window{
-public:
-	Window();
-	Window(int width, int height, std::string title);
-	~Window();
-	void createWindow(int width, int height, const char* title);
-	void closeWindow();
-	void setTitle(std::string title);
-	void setSize(int width, int height);
-	void setPosition(int x, int y);
-	void setFullscreen(bool fullscreen);
-	void setResizable(bool resizable);
-	void setVSync(bool vsync);
-	void setIcon(const char* iconPath);
-	void setBackgroundColor(float r, float g, float b, float a);
-	void setMouseCursorVisible(bool visible);
-	void setMouseCursorPosition(int x, int y);
-	void setMouseCursorGrabbed(bool grabbed);
-	void setMouseCursorScale(float scale);
-	void setMouseCursorHidden(bool hidden);
+#endif  
 
-private:
-	int width;
-	int height;
-	std::string title;
-	bool fullscreen;
-	bool resizable;
-	bool vsync;
-	float backgroundColor[4];
-	bool mouseCursorVisible;
-	int mouseCursorPosition[2];
-	bool mouseCursorGrabbed;
-	float mouseCursorScale;
-	bool mouseCursorHidden;
-	// Add any other necessary private members or methods here
+#include <iostream>  
+class Window {  
+public:  
+   Window();  
+   Window(int width, int height, std::string title, sf::RenderWindow& window);  
+   ~Window();  
+   sf::RenderWindow& createWindow(int width, int height, std::string title, sf::RenderWindow& window);  
+   void setTitle(std::string title);  
+   void setSize(int width, int height);  
+   void setPosition(int x, int y);  
+   void setFullscreen(bool fullscreen);  
+   void setResizable(bool resizable);  
+   void setVSync(bool vsync);  
+   void setIcon(const char* iconPath);  
+   void setBackgroundColor(float r, float g, float b, float a);  
+   void setMouseCursorVisible(bool visible);  
+   void setMouseCursorPosition(int x, int y);  
+   void setMouseCursorGrabbed(bool grabbed);  
+   void setMouseCursorScale(float scale);  
+   void setMouseCursorHidden(bool hidden);  
 
-
-
-};
-
-
+private:  
+   int m_width;  
+   int m_height;  
+   sf::RenderWindow& m_window;  
+   std::string m_title;  
+   bool m_fullscreen;  
+   bool m_resizable;  
+   bool m_vsync;  
+   float m_backgroundColor[4];  
+   bool m_mouseCursorVisible;  
+   int m_mouseCursorPosition[2];  
+   bool m_mouseCursorGrabbed;  
+   float m_mouseCursorScale;  
+   bool m_mouseCursorHidden;  
+   // Add any other necessary private members or methods here  
+};  
 
 #endif // !WINDOW_H
-
-
-
